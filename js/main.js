63e5cc7876e0562af6ba1c79352a15d5fa9687ed
= <43546339+mohammedaniz@users.noreply.github.com>
2020-06-29 12:21:11 +0530

    Responsive Sidebar with toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+
+// initial hidden elements Setup
 $('#drawer').hide();
+
+//leaflet setup
 let mapId = document.getElementById('mapId');
 var mymap = L.map('mapId').setView([51.505, -0.09], 13);
-
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -15,45 +17,34 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     var marker = L.marker([51.5, -0.09]).addTo(mymap);
     var popup = L.popup();
 
-    marker.bindPopup("<ul class='list-group list-group-flush'><li class='list-group-item'>First item</li><li class='list-group-item'>Second item</li><li class='list-group-item'>Third item</li><li class='list-group-item'>First item</li><li class='list-group-item'>Second item</li><li class='list-group-item'>Third item</li></ul>").openPopup();
+    marker.bindPopup("<ul class='list-group list-group-flush'><li class='list-group-item' onclick='openDrawer(1)'>First item</li><li onclick='openDrawer(2)' class='list-group-item'>Second item</li><li onclick='openDrawer(3)' class='list-group-item'>Third item</li><li onclick='openDrawer(4)' class='list-group-item'>First item</li><li class='list-group-item' onclick='openDrawer(5)'>Second item</li><li class='list-group-item' onclick='openDrawer(6)'>Third item</li></ul>").openPopup();
 
-$('#toggleSidebar').on('click',() => {
-    console.log($('#drawer'))
-   if($('#drawer').hasClass('active')){ 
-    $('#drawer').removeClass('active');
-    $('#drawer').hide(500);
-   }else {
-       $('#drawer').addClass('active');
-       $('#drawer').show(500);
-   }
-});
-function on() {
-   document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-   document.getElementById("overlay").style.display = "none";
-  }
-  
-  $('.list-group-item').on('click',() => {
+
+// toggle side drawer function    
+function openDrawer(id){
+    console.log('Clicked ', id)
     if( $('#drawer').hasClass('active')){
+        console.log('active')
         $('#drawer').removeClass('active');
         $('#drawer').hide(500);
         document.getElementById("overlay").style.display = "none";
     }else {
+        console.log('not active');
         $('#drawer').addClass('active');
         $('#drawer').show(500);
         document.getElementById("overlay").style.display = "block";
     }
-          
-  
-  });
+}
+
+
+//toggle overlay setup
+
 $('#overlay').on('click',(e) => {
    let isDrawerActive = $('#drawer').hasClass('active');
    if(isDrawerActive){
     $('#drawer').removeClass('active');
     $('#drawer').hide(500);
-    off();
+    document.getElementById("overlay").style.display = "none";
    }
 });
 
